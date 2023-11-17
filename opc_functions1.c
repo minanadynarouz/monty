6c4cc65939c5b1a93ec9c736c5c08032ed19942c
minanadynarouz <mina.n.narouz@gmail.com>
2023-11-16 18:58:11 -0800

    amend

--- opc_functions1.c
+++ opc_functions1.c
@@ -129,3 +129,33 @@ void div(stack_t **dll, unsigned int line_num)
         tmp->n = tmp->n / (*dll)->n;
         pop(dll, line_num);
 }
+
+/**
+ * mul - mul top 2 element together
+ * @dll: doubly linked list
+ * @line_num: line number
+ */
+
+void mul(stack_t **dll, unsigned int line_num)
+{
+        int i;
+        stack_t *tmp;
+
+        tmp = *dll;
+
+        i = 0;
+        while (tmp != NULL)
+        {
+                tmp = tmp->next;
+                i++;
+        }
+
+        if (i < 2)
+        {
+                fprintf(stderr, "L%u: can't mul, stack too short\n", line_num);
+                exit(EXIT_FAILURE);
+        }
+        tmp = (*dll)->next;
+        tmp->n = tmp->n * (*dll)->n;
+        pop(dll, line_num);
+}
