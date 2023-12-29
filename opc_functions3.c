91492c542020a1272c9920577e6c85aa6bf4992b
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-29 00:27:08 -0800

    amend

--- opc_functions3.c
+++ opc_functions3.c
@@ -26,3 +26,27 @@ void rotl(stack_t **dll, unsigned int __attribute__((__unused__)) line_num)
 	tempLast->next = NULL;
 	tempLast->prev = temp;
 }
+
+/**
+ * rotr - rotate the stack to the top
+ * @dll: doubly linked list
+ * @line_num: line number
+ */
+
+void rotr(stack_t **dll, unsigned int __attribute__((__unused__)) line_num)
+{
+	stack_t *head, *last;
+
+	if (*dll == NULL || (*dll)->next == NULL)
+		return;
+	
+	last = *dll;
+
+	while ((*dll)->next != NULL)
+	{
+		*dll = (*dll)->next;
+	}
+	head = *dll;
+	head->prev = NULL;
+	last->next = NULL;
+}
